7c7ac3db57f69120e1766632f92ef81999b95746
julio courbis <courbisweb@gmail.com>
2019-04-11 00:45:52 -0400

    se agrega panel de recepcion y jquery-ui

--- clienteWeb/js/home.js
+++ clienteWeb/js/home.js
@@ -6,5 +6,30 @@ $("#recepcionVale").hover(function(){
 	$(this).css("cursor","pointer");
 });	
 
+$("#recepcionVale").click(function(){
+	$(this).toggle("clip",1000);
+	$("#panelRecepcionVale").toggle("slide",5000);
+});	
+
+
+$("#salirPanelRecepcionVale").click(function(){
+	$("#panelRecepcionVale").toggle("clip");
+	$("#recepcionVale").toggle("slide",2000);
+});	
+
+$("#verDetalle").hover(function(){
+	console.log("Estoy dentro del div");
+	$(this).css("cursor","pointer");
+});	
+
+$("#salirPanelRecepcionVale").hover(function(){
+	console.log("Estoy dentro del div");
+	$(this).css("cursor","pointer");
+});	
+
+$("#panelRecepcionVale").tabs();
+
 });
+
+
 
